248a7590f27975d516cd8e4d8e876fccbb5b1245
Kimginam97 <rlarlska79@naver.com>
2022-09-06 10:16:52 +0900

    객체 생성하기

--- ch02(기초)/basics-01-starting-project/assets/scripts/app.js
+++ ch02(기초)/basics-01-starting-project/assets/scripts/app.js
@@ -19,8 +19,14 @@ function add() {
     currentResult += enteredNumber;
     // currentResult++;
     createAndWriteOutput('+', initialResult, enteredNumber); // from vendor file
-    logEntries.push(enteredNumber);
-    console.log(logEntries[0]);
+    const logEntry = {
+        operation: 'ADD',
+        prevResult: initialResult,
+        number: enteredNumber,
+        result: currentResult,
+    };
+    logEntries.push(logEntry);
+    console.log(logEntries);
 }
 
 function subtract() {
