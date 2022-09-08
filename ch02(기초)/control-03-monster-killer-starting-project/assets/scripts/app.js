19878cf4593cc433ffc2bfb0fe7ee0895f7f02fc
Kimginam97 <rlarlska79@naver.com>
2022-09-08 15:29:54 +0900

    try-catch 사용해서 오류잡고 처리하기

--- ch02(기초)/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ ch02(기초)/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -23,9 +23,15 @@ function getMaxLifeValues() {
     }
     return parsedValue;
 }
-
-let chosenMaxLife = getMaxLifeValues();
-
+let chosenMaxLife;
+try {
+    chosenMaxLife = getMaxLifeValues();
+} catch (error) {
+    console.log(error);
+    chosenMaxLife = 100;
+    alert('You entered something wrong, default value of 100 was used.');
+    // throw error;
+}
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 let hasBonusLife = true;
