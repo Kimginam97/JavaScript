36436d93ed8ab350e7320b5e584a03a7a23ab1e1
Kimginam97 <rlarlska79@naver.com>
2022-09-13 08:52:14 +0900

    Var vs let & const - 블록범위

---
+++ ch04(ES6)/behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,23 @@
+let name = 'Max';
+
+{
+    let test = 5;
+    console.log(test);
+}
+
+// console.log(test); 오류
+
+if (name === 'Max') {
+    let hobbies = ['Sports', 'Cooking'];
+    console.log(hobbies);
+}
+
+function greet() {
+    let age = 30;
+    let name = 'Manuel';
+    console.log(name, age);
+}
+
+// console.log(name, hobbies); 오류
+
+greet();
