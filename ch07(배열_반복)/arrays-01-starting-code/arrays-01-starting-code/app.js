744036cbdd8e90de685950e80be8343bbb07f676
Kimginam97 <rlarlska79@naver.com>
2023-01-02 10:34:09 +0900

    indexof(), lastIndexof()로 인덱스 회수

--- ch07(배열_반복)/arrays-01-starting-code/arrays-01-starting-code/app.js
+++ ch07(배열_반복)/arrays-01-starting-code/arrays-01-starting-code/app.js
@@ -43,10 +43,15 @@
 // const removedElements = hobbies.splice(-2, 1);
 // console.log(hobbies);
 
-const testResults = [1, 5.3, 1.5, 10.99, -5, 10];
+const testResults = [1, 5.3, 1.5, 10.99, 1.5, -5, 10];
 // const storedResults = testResults.splice(2); //새 복사본에 저장
 const storedResults = testResults.concat([3.99, 2]);
 
 testResults.push(5.91);
 
 console.log(storedResults, testResults);
+console.log(testResults.indexOf(1.5));
+console.log(testResults.lastIndexOf(1.5));
+
+const personalData = [{ name: 'Max' }, { name: 'Manuel' }];
+console.log(personalData.indexOf({ name: 'Manuel' }));
