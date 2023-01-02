612d22c612888b64742651e1f63de4ffebc2ad1e
Kimginam97 <rlarlska79@naver.com>
2023-01-02 09:57:59 +0900

    push, pop, unshift, shift 배열요소 추가및 제거

--- ch07(배열_반복)/arrays-01-starting-code/arrays-01-starting-code/app.js
+++ ch07(배열_반복)/arrays-01-starting-code/arrays-01-starting-code/app.js
@@ -1,27 +1,39 @@
-const numbers = [1, 2, 3];
-console.log(numbers);
+// const numbers = [1, 2, 3];
+// console.log(numbers);
 
-// const moreNumbers = new Array(5,2);
-// console.log(moreNumbers);
+// // const moreNumbers = new Array(5,2);
+// // console.log(moreNumbers);
 
-// const yetMoreNumbers = Array.of(1, 2);
-// console.log(yetMoreNumbers);
+// // const yetMoreNumbers = Array.of(1, 2);
+// // console.log(yetMoreNumbers);
 
-const listItems = document.querySelectorAll('li');
-console.log(listItems);
+// const listItems = document.querySelectorAll('li');
+// console.log(listItems);
 
-const arrayListItems = Array.from("Hi!");
-console.log(arrayListItems);
+// const arrayListItems = Array.from("Hi!");
+// console.log(arrayListItems);
 
-const hobbies = ['Cooking', 'Sports'];
-const personalData = [30, 'Max', { moreDetail: [] }];
+// const hobbies = ['Cooking', 'Sports'];
+// const personalData = [30, 'Max', { moreDetail: [] }];
 
-const analyticsData = [[1, 1.6], [-5.4, 2.1]];
+// const analyticsData = [[1, 1.6], [-5.4, 2.1]];
 
-for (const data of analyticsData) {
-    for (const dataPoint of data) {
-        console.log(dataPoint);
-    }
-}
+// for (const data of analyticsData) {
+//     for (const dataPoint of data) {
+//         console.log(dataPoint);
+//     }
+// }
 
-console.log(personalData[2]);
+// console.log(personalData[2]);
+
+const hobbies = ['Sports', 'Cooking'];
+hobbies.push('Reading');
+hobbies.unshift('Coding'); // 모든요소를 오른쪽
+const poppedValue = hobbies.pop();
+hobbies.shift(); // 모든요소를 왼쪽 첫번째는 삭제
+console.log(hobbies);
+
+hobbies[1] = 'Coding';
+hobbies[5] = 'Reading';
+console.log(hobbies);
+
