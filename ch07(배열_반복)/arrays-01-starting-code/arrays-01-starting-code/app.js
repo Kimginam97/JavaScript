dfd6092327cb74e4c9d82f9a4a1e774ee823e18c
Kimginam97 <rlarlska79@naver.com>
2023-01-02 11:34:43 +0900

    화살표 함수의 장점

--- ch07(배열_반복)/arrays-01-starting-code/arrays-01-starting-code/app.js
+++ ch07(배열_반복)/arrays-01-starting-code/arrays-01-starting-code/app.js
@@ -109,8 +109,10 @@ const sortedPrices = prices.sort((a, b) => {
 // console.log(sortedPrices);
 // console.log(sortedPrices.reverse());
 
-const filteredArray = prices.filter((price, index, prices) => {
-    return price > 6;
-});
+// const filteredArray = prices.filter((price, index, prices) => {
+//     return price > 6;
+// });
+
+const filteredArray = prices.filter(price => price > 6);
 
 console.log(filteredArray);
