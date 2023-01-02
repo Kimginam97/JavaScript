95a4bb14356009870d7ad7776b62fe1243c5236f
Kimginam97 <rlarlska79@naver.com>
2023-01-02 14:02:21 +0900

    Maps 작업

--- ch07(배열_반복)/arrays-01-starting-code/arrays-01-starting-code/setsmaps.js
+++ ch07(배열_반복)/arrays-01-starting-code/arrays-01-starting-code/setsmaps.js
@@ -1,11 +1,37 @@
-const ids = new Set(['hi', 'from', 'set']);
-ids.add(2);
+// const ids = new Set(['hi', 'from', 'set']);
+// ids.add(2);
 
-if (ids.has('hi')) {
-    ids.delete('hi');
+// if (ids.has('hi')) {
+//     ids.delete('hi');
+// }
+
+
+// for (const entry of ids.entries()) {
+//     console.log(entry);
+// };
+
+
+const person1 = { name: 'Max' };
+const person2 = { name: 'Manuel' };
+
+const personData = new Map([[person1, [{ date: 'yesterday', price: 10 }]]]);
+
+personData.set(person2, [{ date: 'two weeks ago', price: 100 }]);
+
+console.log(personData);
+console.log(personData.get(person1));
+
+for (const [key, value] of personData.entries()) {
+    console.log(key, value);
+}
+
+for (const key of personData.keys()) {
+    console.log(key);
+}
+
+for (const value of personData.values()) {
+    console.log(value);
 }
 
 
-for (const entry of ids.entries()) {
-    console.log(entry);
-};
+console.log(personData.size)
