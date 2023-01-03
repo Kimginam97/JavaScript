779e88f1aa26ebd3efb638e8e8ea141e20eec4f6
Kimginam97 <rlarlska79@naver.com>
2023-01-03 09:58:37 +0900

    프로퍼티 유형 & 순서

--- ch08(객체)/objects-01-starting-project/objects-01-starting-project/assets/scripts/objects.js
+++ ch08(객체)/objects-01-starting-project/objects-01-starting-project/assets/scripts/objects.js
@@ -9,7 +9,8 @@ let person = {
     hobbies: ['Sports', 'Cooking'],
     greet: function () {
         alert('Hi there!');
-    }
+    },
+    1.5: 'hello'
 };
 
 // person.age = 31;
@@ -19,3 +20,5 @@ delete person.age;
 person.isAdmin = true;
 
 console.log(person['first name']);
+console.log(person[1.5]);
+console.log(person);
