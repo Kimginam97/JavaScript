61c7bc1e2ae2c56d8bec43b4ff5a07b0e85d3900
Kimginam97 <rlarlska79@naver.com>
2023-01-04 10:10:19 +0900

    생성자 메서드로 작업

--- ch09(OOP)/oop-01-starting-setup/assets/scripts/app.js
+++ ch09(OOP)/oop-01-starting-setup/assets/scripts/app.js
@@ -3,25 +3,31 @@ class Product {
     imageUrl;
     description;
     price;
+
+    constructor(title, image, desc, price) {
+        this.title = title;
+        this.imageUrl = image;
+        this.description = desc;
+        this.price = price;
+    }
 }
 
 console.log(new Product());
 
 const productList = {
     products: [
-        // new Product()
-        {
-            title: 'A Pillow',
-            imageUrl: 'https://www.maxpixel.net/static/photo/2x/Soft-Pillow-Green-Decoration-Deco-Snuggle-1241878.jpg',
-            price: 19.99,
-            description: 'A soft pillow!'
-        },
-        {
-            title: 'A Carpet',
-            imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg',
-            price: 89.99, 
-            description: 'A carpet which you might like - or not.',
-        }
+        new Product(
+            'A Pillow',
+            'https://www.maxpixel.net/static/photo/2x/Soft-Pillow-Green-Decoration-Deco-Snuggle-1241878.jpg',
+            'A soft pillow!',
+            19.99
+        ),
+        new Product(
+            'A Carpet',
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg',
+            'A carpet which you might like - or not.',
+            89.99, 
+        ),
     ],
     render() {
         const renderHook = document.getElementById('app');
