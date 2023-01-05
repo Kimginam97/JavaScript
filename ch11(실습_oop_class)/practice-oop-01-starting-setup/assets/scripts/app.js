021d79d476b81d644129434869c14722bd472664
Kimginam97 <rlarlska79@naver.com>
2023-01-05 13:42:04 +0900

    프로젝트 리스트 생성 & 요소 데이터 구문 분석

---
+++ ch11(실습_oop_class)/practice-oop-01-starting-setup/assets/scripts/app.js
@@ -0,0 +1,20 @@
+class Tooltip{ }
+
+class ProjectItem { }
+
+class ProjectList { 
+    constructor(type) {
+        const prjItems = document.querySelectorAll(`#${type}-projects`); 
+        console.log(prjItems);
+    }
+}
+
+class App { 
+    static init() {
+        const activeProjectsList = new ProjectList('active');
+        const finishedProjectsList = new ProjectList('finished');
+    }
+}
+
+App.init();
+
