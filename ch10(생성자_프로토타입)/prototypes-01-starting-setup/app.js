33749d9717dc54722dc1d3ef137ce7d7c8c6994d
Kimginam97 <rlarlska79@naver.com>
2023-01-05 10:51:22 +0900

    프로토타입 체인과 전역 객체

--- ch10(생성자_프로토타입)/prototypes-01-starting-setup/app.js
+++ ch10(생성자_프로토타입)/prototypes-01-starting-setup/app.js
@@ -1,36 +1,48 @@
-// class Person {
-//     name = 'Max';
+class AgedPerson{
+    printAge() {
+        console.log(this.age);
+    }
+}
+class Person extends AgedPerson{
+    name = 'Max';
 
-//     constructor() {
-//         this.age = 30;
-//     }
+    constructor() {
+        super();
+        this.age = 30;
+    }
 
-//     greet() {
-//         console.log(
-//             'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
-//         );
-//     }
-// }
-
-function Person() {
-    this.age = 30;
-    this.name = "Max";
-    this.greet = function () {
+    greet() {
         console.log(
             'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
         );
     }
 }
 
-Person.prototype = {
-    printAge() {
-        console.log(this.age);
-    }
+// function Person() {
+//     this.age = 30;
+//     this.name = "Max";
+//     this.greet = function () {
+//         console.log(
+//             'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
+//         );
+//     }
+// }
+
+Person.prototype.printAge = function () {
+    console.log(this.age);
+};
+
+Person.describe = function () {
+    console.log('Creating persons...');
 }
 
 console.dir(Person);
+console.log(Object);
 
 const p = new Person();
 p.greet();
 p.printAge();
-console.log(p.__proto__);
+console.log(p.lenth);
+console.log(p.toString());
+const p2 = new p.__proto__.constructor();
+console.log(Object.prototype.__proto__);
