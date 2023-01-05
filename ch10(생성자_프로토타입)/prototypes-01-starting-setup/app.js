f764a38caeb6c2175da4bd6aede71ff86f6dbe80
Kimginam97 <rlarlska79@naver.com>
2023-01-05 13:22:47 +0900

    프로토타입 Setting & Getting

--- ch10(생성자_프로토타입)/prototypes-01-starting-setup/app.js
+++ ch10(생성자_프로토타입)/prototypes-01-starting-setup/app.js
@@ -1,51 +1,113 @@
-class AgedPerson{
+class AgedPerson {
     printAge() {
-        console.log(this.age);
+    console.log(this.age);
     }
 }
-class Person extends AgedPerson{
+
+class Person {
     name = 'Max';
 
     constructor() {
-        super();
-        this.age = 30;
+      // super();
+    this.age = 30;
+      // this.greet = function() { ... }
     }
+
+    // greet = () => {
+    //   console.log(
+    //     'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
+    //   );
+    // };
 
     greet() {
         console.log(
-            'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
-        );
+        'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
+    );
     }
 }
 
-// function Person() {
-//     this.age = 30;
-//     this.name = "Max";
-//     this.greet = function () {
-//         console.log(
-//             'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
-//         );
-//     }
-// }
+  // function Person() {
+  //   this.age = 30;
+  //   this.name = 'Max';
+  //   // this.greet = function() { ... };
+  // }
 
-// Person.prototype.printAge = function () {
-//     console.log(this.age);
-// };
+  // Person.prototype.greet = function() {
+  //   console.log(
+  //     'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
+  //   );
+  // };
 
-// Person.describe = function () {
-//     console.log('Creating persons...');
-// }
+  // Person.describe = function() {
+  //   console.log('Creating persons...');
+  // }
 
-// console.dir(Person);
-// console.log(Object);
+  // Person.prototype = {
+  //   printAge() {
+  //     console.log(this.age);
+  //   }
+  // };
 
-// const p = new Person();
-// p.greet();
-// p.printAge();
-// console.log(p.lenth);
-// console.log(p.toString());
-// const p2 = new p.__proto__.constructor();
-// console.log(Object.prototype.__proto__);
+  // Person.prototype.printAge = function() {
+  //   console.log(this.age);
+  // };
 
-const p = new Person();
-console.log(p);
+  // console.dir(Person);
+
+  // const p = new Person();
+  // p.greet();
+  // p.printAge();
+  // console.log(p.length);
+  // console.log(p.toString());
+  // const p2 = new p.__proto__.constructor();
+  // console.dir(Object.prototype.__proto__);
+
+  // const p = new Person();
+  // const p2 = new Person();
+  // p.greet();
+  // console.log(p);
+
+  // const button = document.getElementById('btn');
+  // button.addEventListener('click', p.greet.bind(p));
+
+const course = {
+    // new Object()
+    title: 'JavaScript - The Complete Guide',
+    rating: 5
+};
+
+  // console.log(Object.getPrototypeOf(course));
+Object.setPrototypeOf(course, {
+    // ...Object.getPrototypeOf(course),
+    printRating: function() {
+        console.log(`${this.rating}/5`);
+    }
+});
+
+const student = Object.create({
+    printProgress: function() {
+        console.log(this.progress);
+    }
+}, {
+    name: {
+        configurable: true,
+        enumerable: true,
+        value: 'Max',
+        writable: true
+    }
+});
+
+  // student.name = 'Max';
+
+Object.defineProperty(student, 'progress', {
+    configurable: true,
+    enumerable: true,
+    value: 0.8,
+    writable: false
+});
+
+student.printProgress();
+
+console.log(student);
+
+course.printRating();
