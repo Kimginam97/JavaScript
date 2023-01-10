ef69adb8092d5a87e9d944c864c5640a2b76f8ed
Kimginam97 <rlarlska79@naver.com>
2023-01-10 14:20:18 +0900

    이벤트 전파 & stopPropagation()

--- ch13(이벤트작업)/events-01-starting-setup/assets/scripts/events.js
+++ ch13(이벤트작업)/events-01-starting-setup/assets/scripts/events.js
@@ -1,11 +1,11 @@
-const buttons = document.querySelectorAll('button');
+const button = document.querySelector('button');
 
 const buttonClickHandler = event => {
-    event.target.disabled = true;
+    // event.target.disabled = true;
     console.log(event);
 };
 
-const boundFn = buttonClickHandler.bind(this);
+// const boundFn = buttonClickHandler.bind(this);
 
 // button.addEventListener('click', buttonClickHandler);
 
@@ -13,9 +13,9 @@ const boundFn = buttonClickHandler.bind(this);
 //     button.removeEventListener('click',buttonClickHandler);
 // },2000)
 
-buttons.forEach(btn => {
-    btn.addEventListener('click', buttonClickHandler);
-})
+// buttons.forEach(btn => {
+//     btn.addEventListener('click', buttonClickHandler);
+// })
 
 // window.addEventListener('scroll', event => {
 //     console.log(event);
@@ -25,5 +25,19 @@ const form = document.querySelector('form');
 
 form.addEventListener('submit', event => {
     event.preventDefault(); // 페이지 새로고침 중단
+    console.log(event);
+})
+
+const div = document.querySelector('div');
+
+div.addEventListener('click', event => {
+    console.log('CLICKED DIV');
+    console.log(event);
+})
+
+button.addEventListener('click', event => {
+    event.stopPropagation(); //상위요소 클릭리스너 실행x
+    // event.stopImmediatePropagation(); // 동일요소 클릭리스너 실행 x
+    console.log('CLICKED BUTTON');
     console.log(event);
 })
