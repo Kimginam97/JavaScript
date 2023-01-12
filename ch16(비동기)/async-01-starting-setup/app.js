53b9477dc090220e880c0784e5130ecf4706c96b
Kimginam97 <rlarlska79@naver.com>
2023-01-12 15:58:10 +0900

    Async / Await vs Raw 프로미스

--- ch16(비동기)/async-01-starting-setup/app.js
+++ ch16(비동기)/async-01-starting-setup/app.js
@@ -37,23 +37,28 @@ async function trackUserHandler() {
   }
 
   console.log(posData, timerData);
-    // .then(posData => {
-    //   positionData = posData;
-    //   return setTimer(2000);
-    // })
-    // .catch(err => {
-    //   console.log(err);
-    //   return 'on we go...';
-    // })
-    // .then(data => {
-    //   console.log(data, positionData);
-    // })
-    // .catch()
-  // setTimer(1000).then(() => {
-  //   console.log('Timer done!');
-  // })
-  // console.log('Getting position...');
+  // getPosition()
+  //   .then(posData => {
+  //     positionData = posData;
+  //     return setTimer(2000);
+  //   })
+  //   .catch(err => {
+  //     console.log(err);
+  //     return 'on we go...';
+  //   })
+  //   .then(data => {
+  //     console.log(data, positionData);
+  //   })
+  //   .catch()
+  setTimer(1000).then(() => {
+    console.log('Timer done!');
+  })
+  console.log('Getting position...');
 }
+
+(async function () {
+  await setTimer(1000);
+})();
 
 button.addEventListener('click', trackUserHandler);
 
