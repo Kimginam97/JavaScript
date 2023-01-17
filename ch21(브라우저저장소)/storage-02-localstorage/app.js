2113780709980f6fa442f7393cf33e256930e78f
Kimginam97 <rlarlska79@naver.com>
2023-01-17 17:12:57 +0900

    IndexedDB 시작하기

--- ch21(브라우저저장소)/storage-02-localstorage/app.js
+++ ch21(브라우저저장소)/storage-02-localstorage/app.js
@@ -1,17 +1,32 @@
 const storeBtn = document.getElementById('store-btn');
 const retrBtn = document.getElementById('retrieve-btn');
 
+const dbRequest = indexedDB.open('StorageDummy', 1);
+
+dbRequest.onupgradeneeded = function (event) {
+    const db = event.target.result;
+
+    const objStore = db.createObjectStore('products', { keyPath: 'id' });
+    
+    objStore.transaction.oncomplete = function (event) {
+        const productsStore = db.transaction('products', 'readwrite').objectStore('products');
+        productsStore.add({
+            id: 'p1',
+            title: 'A First Product',
+            price: 12.99,
+            tags: ['Expensive', 'Luxury']
+        });
+    }
+};
+
+dbRequest.onerror = function (event) {
+    console.log('ERROR!');
+};
+
 storeBtn.addEventListener('click', () => {
-    const userId = 'u123';
-    const user = { name: 'Max', age: 30 };
-    document.cookie = `uid=${userId}; max-age=360`;
-    document.cookie = `user=${JSON.stringify(user)}`;
+
 });
 
 retrBtn.addEventListener('click', () => {
-    const cookieData = document.cookie.split(';');
-    const data = cookieData.map(i => {
-        return i.trim();
-    })
-    console.log(data[1].split('=')[1]);
+
 });
