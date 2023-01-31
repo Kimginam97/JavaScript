0718b6670188d31e27abc157f944c3259bf6881e
Kimginam97 <rlarlska79@naver.com>
2023-01-31 13:54:09 +0900

    잘 알려진 기호들

--- ch25(메타_프로그래밍)/meta-01-starting-setup/app.js
+++ ch25(메타_프로그래밍)/meta-01-starting-setup/app.js
@@ -4,10 +4,15 @@ console.log(uid);
 const user = {
     // id: 'p1'
     [uid]: 'p1',
-    name: 'Max'
-    age: 30
+    name: 'Max',
+    age: 30,
+    [Symbol.toStringTag]: 'User Object'
 };
+
+user[uid] = 'p3';
 
 user.id = 'p2';
 
-console.log(user);
+console.log(user[Symbol('uid')]);
+console.log(Symbol('uid') === Symbol('uid'));
+console.log(user.toString());
